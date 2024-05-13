57dfdfc8df864b6cbbfc10cdace059a346aab6e2
JeinKh <154812936+JeinKh@users.noreply.github.com>
2024-05-13 14:28:52 +0000

    Deploying to gh-pages from @ JeinKh/goit-js-hw-09@6e0f6724fcfcf7c4920a21a6d87fd7d9ebeafa00 🚀

---
+++ commonHelpers.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";import{S as t}from"./assets/vendor-10cb7c31.js";document.addEventListener("DOMContentLoaded",function(){new t(".gallery a",{captionsData:"alt",captionDelay:250})});
+//# sourceMappingURL=commonHelpers.js.map
